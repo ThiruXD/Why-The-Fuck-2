98b2a42ec11cb9692cf66bab566a9ffd313688d2
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-23 18:48:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Thiruselvan999/Why-The-Fuck-2",
+    "https://github.com/AnonymousR1025/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
